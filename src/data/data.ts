921dc0f31357856069086fc9b3fa653642643619
francogarzon-mm <franco.garzon@mediamonks.com>
2025-07-15 11:21:14 -0300

    Updated deploy github pages

--- src/data/data.ts
+++ src/data/data.ts
@@ -1,7 +1,7 @@
 const data = [
   {
     id: 1,
-    url: "src/assets/slider-images/laika-img1.webp",
+    url: "images/laika-img1.webp",
     alt: "laika img1",
     content: {
       text: "Coraline Curious Creator Kit",
@@ -17,7 +17,7 @@ const data = [
   },
   {
     id: 2,
-    url: "src/assets/slider-images/laika-img2.webp",
+    url: "images/laika-img2.webp",
     alt: "laika img2",
     content: {
       text: "ParaNorman Cat Toy Set",
@@ -33,7 +33,7 @@ const data = [
   },
   {
     id: 3,
-    url: "src/assets/slider-images/laika-img2.webp",
+    url: "images/laika-img2.webp",
     alt: "laika img2",
     content: {
       text: "Coraline Pink Palace Enamel Pin",
@@ -49,7 +49,7 @@ const data = [
   },
   {
     id: 4,
-    url: "src/assets/slider-images/laika-img1.webp",
+    url: "images/laika-img1.webp",
     alt: "laika img3",
     content: {
       text: "ParaNorman Cat Toy Set",
@@ -65,7 +65,7 @@ const data = [
   },
   {
     id: 5,
-    url: "src/assets/slider-images/laika-img3.webp",
+    url: "images/laika-img3.webp",
     alt: "laika img3",
     content: {
       text: "Coraline's Other World 3D Viewer",
@@ -81,7 +81,7 @@ const data = [
   },
   {
     id: 6,
-    url: "src/assets/slider-images/laika-img2.webp",
+    url: "images/laika-img2.webp",
     alt: "laika img3",
     content: {
       text: "Coraline's Other World 3D Viewer",
@@ -97,7 +97,7 @@ const data = [
   },
   {
     id: 7,
-    url: "src/assets/slider-images/laika-img1.webp",
+    url: "images/laika-img1.webp",
     alt: "laika img3",
     content: {
       text: "ParaNorman Zombie Collar",
@@ -159,50 +159,50 @@ const dataWithoutImg = [
       link: "#",
     },
   },
-    {
-      id: 4,
-      alt: "laika img3",
-      content: {
-        text: "ParaNorman Cat Toy Set",
-      },
-      categories: [
-        { text: "Arts & Crafts", highlight: true },
-        { text: "Book", highlight: false },
-      ],
-      cta: {
-        text: "VIEW PRODUCT",
-        link: "#",
-      },
+  {
+    id: 4,
+    alt: "laika img3",
+    content: {
+      text: "ParaNorman Cat Toy Set",
     },
-    {
-      id: 5,
-      alt: "laika img3",
-      content: {
-        text: "Coraline's Other World 3D Viewer",
-      },
-      categories: [
-        { text: "Arts & Crafts", highlight: true },
-        { text: "Book", highlight: false },
-      ],
-      cta: {
-        text: "VIEW PRODUCT",
-        link: "#",
-      },
+    categories: [
+      { text: "Arts & Crafts", highlight: true },
+      { text: "Book", highlight: false },
+    ],
+    cta: {
+      text: "VIEW PRODUCT",
+      link: "#",
     },
-    {
-      id: 6,
-      alt: "laika img3",
-      content: {
-        text: "ParaNorman Zombie Collar",
-      },
-      categories: [
-        { text: "Arts & Crafts", highlight: true },
-        { text: "Book", highlight: false },
-      ],
-      cta: {
-        text: "VIEW PRODUCT",
-        link: "#",
-      },
+  },
+  {
+    id: 5,
+    alt: "laika img3",
+    content: {
+      text: "Coraline's Other World 3D Viewer",
     },
+    categories: [
+      { text: "Arts & Crafts", highlight: true },
+      { text: "Book", highlight: false },
+    ],
+    cta: {
+      text: "VIEW PRODUCT",
+      link: "#",
+    },
+  },
+  {
+    id: 6,
+    alt: "laika img3",
+    content: {
+      text: "ParaNorman Zombie Collar",
+    },
+    categories: [
+      { text: "Arts & Crafts", highlight: true },
+      { text: "Book", highlight: false },
+    ],
+    cta: {
+      text: "VIEW PRODUCT",
+      link: "#",
+    },
+  },
 ];
 export { data, dataWithoutImg };
